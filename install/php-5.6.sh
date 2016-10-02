d41173bdede46875065bc9dba241a67c1d0dada9
Elger van Boxtel <elgervb@users.noreply.github.com>
2016-10-02 23:47:08 +0200

    disable output

--- install/php-5.6.sh
+++ install/php-5.6.sh
@@ -1,7 +1,9 @@
-sudo apt-get install php5
-sudo apt-get install -y php5-gd php5-mcrypt php5-mysql php5-sqlite php-apc 
+echo -e "\n--- Installing PHP with modules ---\n"
+
+sudo apt-get install php5 > /dev/null 2>&1
+sudo apt-get install -y php5-gd php5-mcrypt php5-mysql php5-sqlite php-apc  > /dev/null 2>&1
 php -v
 
 # turn on PHP errors
-sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
-sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
+sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini > /dev/null 2>&1
+sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini > /dev/null 2>&1

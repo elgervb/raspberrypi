70f3838e35a34126dd3ba2c946662253949f57e4
Elger van Boxtel <elgervb@gmail.com>
2016-10-02 21:41:59 +0000

    add sudo

--- install/php-5.6.sh
+++ install/php-5.6.sh
@@ -3,5 +3,5 @@ sudo apt-get install -y php5-gd php5-mcrypt php5-mysql php5-sqlite php-apc
 php -v
 
 # turn on PHP errors
-sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
-sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini
+sudo sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php5/apache2/php.ini
+sudo sed -i "s/display_errors = .*/display_errors = On/" /etc/php5/apache2/php.ini

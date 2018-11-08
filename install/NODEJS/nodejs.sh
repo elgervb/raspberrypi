e85a6a6a0c7ee4093b5b62f51cb9f7a6df04aa33
Elger van Boxtel <elgervb@users.noreply.github.com>
2018-11-08 10:29:57 +0100

    Update nodejs.sh

--- install/NODEJS/nodejs.sh
+++ install/NODEJS/nodejs.sh
@@ -1,6 +1,6 @@
 echo -e "\n--- Installing NodeJS & NPM ---\n"
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash > /dev/null 2>&1
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash > /dev/null 2>&1
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 source ~/.bashrc > /dev/null 2>&1

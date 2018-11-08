aa2071762919cd1901fafabc706bac6d6db85c43
Elger van Boxtel <elgervb@users.noreply.github.com>
2018-11-08 10:55:30 +0100

    Create docker.sh

---
+++ install/DOCKER/docker.sh
@@ -0,0 +1,14 @@
+echo -e "\n--- Installing Docker ---\n"
+
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+
+sudo apt update
+
+sudo apt install -y docker-ce
+
+sudo systemctl status docker
+

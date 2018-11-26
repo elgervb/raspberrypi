1d5bd4f7e3e7e4c49a18015cea27f48fe8690848
Elger van Boxtel <elger@elgervb.localdomain>
2018-11-26 20:24:27 +0100

    Install yarn

--- install/NODEJS/nodejs.sh
+++ install/NODEJS/nodejs.sh
@@ -1,4 +1,4 @@
-echo -e "\n--- Installing NodeJS & NPM ---\n"
+echo -e "\n--- Installing NVM & NodeJS  ---\n"
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash > /dev/null 2>&1
 export NVM_DIR="$HOME/.nvm"

686771b3f32adb9f3eb35ed4115c66a9cf4b5bf6
Elger van Boxtel <elgervb@users.noreply.github.com>
2019-04-23 22:48:03 +0200

    Create install.sh

---
+++ install/MARIADB/install.sh
@@ -0,0 +1,13 @@
+
+ sudo apt update
+
+ sudo apt -y install mariadb-server
+
+
+echo 'Set root password:'
+
+echo 'sudo mysql -u root -p'
+
+echo 'USE mysql;;
+echo 'UPDATE user SET password=PASSWORD('my-password') WHERE User='root' AND Host = 'localhost';
+echo 'FLUSH PRIVILEGES;'

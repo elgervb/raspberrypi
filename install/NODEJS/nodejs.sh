576a43442780bddac391abf08008ce35127473b9
Elger van Boxtel <elgervb@users.noreply.github.com>
2020-06-30 20:03:11 +0200

    Update nodejs.sh

--- install/NODEJS/nodejs.sh
+++ install/NODEJS/nodejs.sh
@@ -1,6 +1,6 @@
 echo -e "\n--- Installing NVM & NodeJS  ---\n"
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash > /dev/null 2>&1
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 source ~/.bashrc > /dev/null 2>&1

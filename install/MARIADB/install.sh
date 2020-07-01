c55d54e768bf14d4f041c3c2d40d87270c3eb99e
Elger van Boxtel <elgervb@users.noreply.github.com>
2020-07-01 14:36:07 +0200

    Update install.sh

--- install/MARIADB/install.sh
+++ install/MARIADB/install.sh
@@ -8,6 +8,6 @@ echo 'Set root password:'
 
 echo 'sudo mysql -u root -p'
 
-echo 'USE mysql;;
+echo 'USE mysql;
 echo 'UPDATE user SET password=PASSWORD('my-password') WHERE User='root' AND Host = 'localhost';
 echo 'FLUSH PRIVILEGES;'
